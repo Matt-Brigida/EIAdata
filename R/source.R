9fd4cfef8fe13f9e65ca0862e9d000031428ea3e
Matt Brigida <matt@complete-markets.com>
2016-08-08 21:58:57 -0400

    Added .getHEIA() to pull hourly data.

--- R/source.R
+++ R/source.R
@@ -11,7 +11,8 @@ getEIA <- function(ID, key){
             "M" = .getMonEIA(ID, key=key),
             "W" = .getWDEIA(ID, key=key),
             "D" = .getWDEIA(ID, key=key),
-            print("ERROR: The last character of your ID is not one of the possible sampling frequencies (A, Q, M, W, or D)"))
+            "H" = .getHEIA(ID, key=key),
+            print("ERROR: The last character of your ID is not one of the possible sampling frequencies (A, Q, M, W, D, or H)"))
  }
         
 
@@ -106,6 +107,45 @@ getEIA <- function(ID, key){
   values <- as.numeric(levels(df[,-1]))[df[,-1]]
 
   xts_data <- xts(values, order.by=date)
+  names(xts_data) <- sapply(strsplit(ID, "-"), paste, collapse = ".")
+
+  temp <- assign(sapply(strsplit(ID, "-"), paste, collapse = "."), xts_data)
+  return(temp)
+}
+
+.getHEIA <- function(ID, key){
+
+  ID <- unlist(strsplit(ID, ";"))
+  key <- unlist(strsplit(key, ";"))
+  url <- paste("http://api.eia.gov/series?series_id=", ID, "&api_key=", key, "&out=xml", sep="" )
+  doc <- xmlParse(file=url, isURL=TRUE)
+  df <- xmlToDataFrame(nodes = XML::getNodeSet(doc, "//data/row"))
+
+### Sort from oldest to newest ----
+  df <- df[ with(df, order(date)), ]
+ 
+  ## will need to split the date to extract the hour ----
+  ## the date/hour is in a unique format 
+  
+  date <- gsub("T", " ", df$date)
+  date <- gsub("Z", ":00:00", date)
+  
+  ## create year-month-day
+  tmpYear <- substr(date, 1, 4)
+  tmpMonth <- substr(date, 5, 6)
+  tmpDay <- substr(date, 7, 8)
+  yearMonthDay <- paste0(tmpYear, "-", tmpMonth, "-", tmpDay)
+  
+  ## create hour:minute:second
+  hourMinuteSecond <- substr(date, 10, 17)
+  
+  ## but what time zone is the data??? Should be added to as.POSIXct below. However EIA may report data without specifying a unique time zone for all data (all data is appropriate local time) and therefore it is left to the user. This latter case is likely.  
+  date <- as.POSIXct(paste(yearMonthDay, hourMinuteSecond, sep = " "), "%Y%m%d %H:%M:%S")
+  values <- as.numeric(levels(df[,-1]))[df[,-1]]
+
+  xts_data <- xts(values, order.by = date)
+  ## removing time zone set in as.POSIXct
+  indexTZ(xts_data) <- ""
   names(xts_data) <- sapply(strsplit(ID, "-"), paste, collapse = ".")
 
   temp <- assign(sapply(strsplit(ID, "-"), paste, collapse = "."), xts_data)
