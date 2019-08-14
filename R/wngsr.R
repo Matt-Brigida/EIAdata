bd97d00b26f482c41e3f4698f4718aadb28f01d4
Matt Brigida <mdbrigida@gmail.com>
2019-08-14 16:32:43 -0400

    updating wngsr for added rows in storage report

--- R/wngsr.R
+++ R/wngsr.R
@@ -2,12 +2,12 @@
 
 wngsr <- function(){
 
-    df <- utils::read.csv(file = "http://ir.eia.gov/ngs/wngsr.csv", header = F, skip = 6, stringsAsFactors = F, nrows = 7)
+    df <- utils::read.csv(file = "http://ir.eia.gov/ngs/wngsr.csv", header = F, skip = 6, stringsAsFactors = F, nrows = 9)
 
     df <- df[, colSums(is.na(df)) < nrow(df)]
     
     names <- as.character(df[1,])[-1]
-    df <- df[2:7,]
+    df <- df[2:9,]
     df <- data.frame(df[,-1], row.names = df[,1])
 
     ## remove comma in thousands and convert from character to numeric ----
